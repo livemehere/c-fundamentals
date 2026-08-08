6678dc675edd671776f6444656d100af5f566fd2
livemehere <rhdxoals@gmail.com>
2026-08-08 21:42:25 +0900

    feat: preprocess.sh single file

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
+set -e
+
 TARGET_DIR=$1
 AUTO_RUN="${2:-}"
 
 echo "================= PREPROCESS START $TARGET_DIR ================="
 
 mkdir -p src/$TARGET_DIR/build
+
+# pre process
 clang \
   -std=c23 \
   -E \
   src/$TARGET_DIR/main.c \
   -o src/$TARGET_DIR/build/main.i
+
+# compile -> assembly
+clang \
+  -std=c23 \
+  -S \
+  src/$TARGET_DIR/build/main.i \
+  -o src/$TARGET_DIR/build/main.s
+
+# assemble -> object
+clang \
+  -c \
+  src/$TARGET_DIR/build/main.s \
+  -o src/$TARGET_DIR/build/main.o
+
+# link
+clang \
+  src/$TARGET_DIR/build/main.o \
+  -o src/$TARGET_DIR/build/main
+
 
 echo "================= PREPROCESS END $TARGET_DIR ================="
