20e00d98ea9b4cd188fbc0b08d78d6af45d9dec2
livemehere <rhdxoals@gmail.com>
2026-08-08 21:05:09 +0900

    feat: update build.sh

--- README.md
+++ README.md
@@ -5,11 +5,14 @@
 ```bash
 clang --version # Apple clang version 17.0.0 (clang-1700.6.3.2)
 lldb --version # lldb-1703.0.236.21
+bear --version # bear 4.2.0
 ```
 
 ## build and run
 
+> `brew install bear` to create `compile_commands.json` automatically.
+
 ```bash
-./scripts/build.sh template --run
+bear -- ./scripts/build.sh template --run
 ./scripts/run.sh template
 ```
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 TARGET_DIR=$1
-AUTO_RUN=$2
+AUTO_RUN="${2:-}"
 
 echo "================= BUILD START $TARGET_DIR ================="
 
 mkdir -p src/$TARGET_DIR/build
-clang src/$TARGET_DIR/main.c -o src/$TARGET_DIR/build/main
+clang \
+  -std=c23 \
+  -Wall \
+  -Wextra \
+  -Wpedantic \
+  -g \
+  src/$TARGET_DIR/main.c \
+  -o src/$TARGET_DIR/build/main
 
 echo "================= BUILD END $TARGET_DIR ================="
 
-
-if [ $AUTO_RUN = "--run" ]; then
+if [ "$AUTO_RUN" = "--run" ]; then
   ./src/$TARGET_DIR/build/main
 fi
