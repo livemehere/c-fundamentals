32d111381dc9856947cc09c23d988e6f7356f18e
livemehere <rhdxoals@gmail.com>
2026-08-10 04:11:16 +0900

    wip(mini-shell): fflush

--- src/6.mini-shell/main.c
+++ src/6.mini-shell/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("hello");
+
+    while (1) {
+        printf("$ ");
+        // fflush(stdout); // activate when bug occurred
+        char* input = NULL;
+        size_t cap = 0;
+        ssize_t len = getline(&input, &cap, stdin);
+
+        if (len == -1) {
+            perror("getline error\n");
+            return 1;
+        }
+
+        printf(">(len : %zu)(len : %zu)(cap : %zu) %s", strlen(input), len, cap, input);
+    }
+
     return 0;
 }
