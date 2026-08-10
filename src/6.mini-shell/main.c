b0420d70b3c782f0db91d74c03cebc99d84ca6ec
livemehere <rhdxoals@gmail.com>
2026-08-11 00:21:30 +0900

    wip(mini-shell): basic

--- src/6.mini-shell/main.c
+++ src/6.mini-shell/main.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-
     while (1) {
         printf("$ ");
-        // fflush(stdout); // activate when bug occurred
-        char* input = NULL;
-        size_t cap = 0;
-        ssize_t len = getline(&input, &cap, stdin);
+        // fflush(stdout);
 
+        char *input = NULL;
+        size_t linecapp = 0;
+        ssize_t len = getline(&input, &linecapp, stdin);
         if (len == -1) {
-            perror("getline error\n");
-            return 1;
+            perror("getline failed\n");
         }
 
-        printf(">(len : %zu)(len : %zu)(cap : %zu) %s", strlen(input), len, cap, input);
-    }
+        // printf("%zu, %p, %s", len, input, input);
+        input[strcspn(input, "\n")] = '\0';
 
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("Fork failed\n");
+            free(input);
+            continue;
+        }
+
+        /* child */
+        if (pid == 0) {
+            char *args[] = {input, NULL};
+            execvp(args[0], args);
+
+            /* rich on failed only */
+            perror("exec failed");
+            _exit(127);
+        }
+
+        /* parent (me) */
+        wait(NULL);
+        free(input);
+    }
     return 0;
 }
